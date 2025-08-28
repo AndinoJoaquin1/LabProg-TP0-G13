ff3094bbd657e3e3616357fe4a5da916f7f536cd
Fernando <fernando.flores@est.fi.uncoma.edu.ar>
2025-08-28 10:35:59 -0300

    Se realiza cambios en clase ProgramaAnalizador

--- concurrencia/ProgramaAnalizador.java
+++ concurrencia/ProgramaAnalizador.java
@@ -1,4 +1,4 @@
-package tp1.cocurrencia;
+package concurrencia;
 
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
@@ -7,26 +7,37 @@ import java.util.concurrent.Future;
 public class ProgramaAnalizador {
     public static void main(String[] args) {
         int bloquesCorruptosTotales = 0;
-        //"disco" fisico
-        int[][] disco = {
-                { 1, 0, 0, 1, 1, 0, 1, 0, 1, 0 },
-                { 1, 1, 1, 0, -1, 0, 1, -1, -1, 1 },
-                { 0, 0, 1, 1, -1, -1, 0, 0, 1, 1 },
-                { 1, 0, -1, 0, 1, 1, 1, 0, 0, 1 },
-                { 0, 1, 1, 0, 0, 1, 0,-1, -1, 0 }
-        };
-        //genera la pool con 3 hilos
+        int[][] disco = new int[10][10];
+
+        // Inicializo el disco para simular un disco con bloques corrompidos (-1),
+        // vacios (0), ocupados (1)
+        for (int i = 0; i < disco.length; i++) {
+            for (int j = 0; j < disco[0].length; j++) {
+                double r = Math.random();
+                if (r < 0.33)
+                    disco[i][j] = -1;
+                else if (r < 0.66)
+                    disco[i][j] = 0;
+                else
+                    disco[i][j] = 1;
+            }
+        }
+
+        // genera la pool con 4 hilos
         ExecutorService executor = Executors.newFixedThreadPool(4);
         for (int i = 0; i < disco.length; i++) {
-            //se ejecuta una tarea la clase AnalizadorDeDiscos
+            // se ejecuta una tarea la clase AnalizadorDeDiscos
             Future<Integer> retorno = executor.submit(new AnalizadorDeDisco(disco[i]));
             try {
-                //se obtiene el valor
+                // se obtiene el valor
                 bloquesCorruptosTotales += retorno.get();
             } catch (Exception e) {
                 System.out.println(e);
             }
         }
         System.out.println("Hay un total de " + bloquesCorruptosTotales + " bloques corruptos");
+
+        // Termino la execución de la pool de hilos
+        executor.shutdown();
     }
 }
